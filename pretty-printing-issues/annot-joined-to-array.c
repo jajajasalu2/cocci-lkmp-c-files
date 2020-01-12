5e2e5467096617ad2fdd651e3d8184f7c01215fc
Jaskaran Singh <jaskaransingh7654321@gmail.com>
2020-01-12 17:10:59 +0530

    Add more pretty-printing-errors
    Signed-off-by: Jaskaran Singh <jaskaransingh7654321@gmail.com>

---
+++ pretty-printing-issues/annot-joined-to-array.c
@@ -0,0 +1,2 @@
+/* kernel/trace/trace_events.c 2928 */
+static char bootup_event_buf[COMMAND_LINE_SIZE] __initdata;
---
+++ pretty-printing-issues/line-break-annot6.c
@@ -0,0 +1,2 @@
+ /* kernel/trace/trace_kprobe.c 102 */
+static nokprobe_inline bool trace_kprobe_has_gone(struct trace_kprobe *tk);
